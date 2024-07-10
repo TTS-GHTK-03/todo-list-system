451861a7af66ba0dbc7bdc152002235c479a3aa1
Linh Dev <nhoquenroi2002@gmail.com>
2024-07-10 16:18:19 +0700

    feat: update logic api register

--- core/core-authentication/src/main/java/org/ghtk/todo_list/repository/AuthAccountRepository.java
+++ core/core-authentication/src/main/java/org/ghtk/todo_list/repository/AuthAccountRepository.java
@@ -3,11 +3,17 @@ package org.ghtk.todo_list.repository;
 import java.util.Optional;
 import org.ghtk.todo_list.entity.AuthAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthAccountRepository extends JpaRepository<AuthAccount, String> {
 
+  @Query("""
+    select a from AuthAccount a 
+    join AuthUser au on au.accountId = a.id
+    where au.id = :userId
+  """)
   Optional<AuthAccount> findFirstByUserId(String userId);
 
   boolean existsByUsername(String username);
--- core/core-authentication/src/main/java/org/ghtk/todo_list/service/AuthAccountService.java
+++ core/core-authentication/src/main/java/org/ghtk/todo_list/service/AuthAccountService.java
@@ -3,6 +3,7 @@ package org.ghtk.todo_list.service;
 import org.ghtk.todo_list.entity.AuthAccount;
 
 public interface AuthAccountService {
+
   AuthAccount findByUserIdWithThrow(String userId);
   AuthAccount create(String username, String password);
 }
