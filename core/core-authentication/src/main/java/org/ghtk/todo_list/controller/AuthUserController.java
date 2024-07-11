132267f3072dca7d9a7f665d5a0c08f939f00706
HanLuan09 <LuanHan1001@gmail.com>
2024-07-11 18:19:50 +0700

    feat: update logic api reset password

--- core/core-authentication/src/main/java/org/ghtk/todo_list/controller/AuthUserController.java
+++ core/core-authentication/src/main/java/org/ghtk/todo_list/controller/AuthUserController.java
@@ -4,10 +4,7 @@ import jakarta.validation.Valid;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.ghtk.todo_list.dto.request.ActiveAccountRequest;
-import org.ghtk.todo_list.dto.request.ForgotPasswordRequest;
-import org.ghtk.todo_list.dto.request.VerifyResetPasswordRequest;
-import org.ghtk.todo_list.dto.request.RegisterRequest;
+import org.ghtk.todo_list.dto.request.*;
 import org.ghtk.todo_list.dto.response.BaseResponse;
 import org.ghtk.todo_list.facade.AuthFacadeService;
 import org.springframework.http.HttpStatus;
@@ -60,6 +57,16 @@ public class AuthUserController {
 
     return BaseResponse.of(HttpStatus.OK.value(), LocalDateTime.now().toString(),
             authFacadeService.verifyResetPassword(request));
+  }
+
+  @PostMapping("/reset-password")
+  @ResponseStatus(HttpStatus.OK)
+  public BaseResponse resetPassword(@RequestBody @Valid ResetPasswordRequest request) {
+    log.info("(resetPassword)request: {}", request);
+    authFacadeService.resetPassword(request);
+
+    return BaseResponse.of(HttpStatus.OK.value(), LocalDateTime.now().toString(),
+            "Reset Password successfully!!");
   }
 
 }
--- core/core-authentication/src/main/java/org/ghtk/todo_list/facade/AuthFacadeService.java
+++ core/core-authentication/src/main/java/org/ghtk/todo_list/facade/AuthFacadeService.java
@@ -1,19 +1,18 @@
 package org.ghtk.todo_list.facade;
 
-import org.ghtk.todo_list.dto.request.ActiveAccountRequest;
-import org.ghtk.todo_list.dto.request.ForgotPasswordRequest;
-import org.ghtk.todo_list.dto.request.VerifyResetPasswordRequest;
-import org.ghtk.todo_list.dto.request.RegisterRequest;
+import org.ghtk.todo_list.dto.request.*;
 import org.ghtk.todo_list.entity.AuthAccount;
 import org.ghtk.todo_list.dto.response.VerifyResetPasswordResponse;
 
 public interface AuthFacadeService {
 
   void register(RegisterRequest request);
-  void activeAccount(ActiveAccountRequest request);
 
+  void activeAccount(ActiveAccountRequest request);
 
   void forgotPassword (ForgotPasswordRequest request);
 
   VerifyResetPasswordResponse verifyResetPassword(VerifyResetPasswordRequest request);
+
+  void resetPassword(ResetPasswordRequest request);
 }
