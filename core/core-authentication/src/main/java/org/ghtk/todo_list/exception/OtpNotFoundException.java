bb7307d3460e2a972589fa1135f8beda8839d5b1
HanLuan09 <LuanHan1001@gmail.com>
2024-07-11 15:58:50 +0700

    feat: update logic api validate otp forgot password(#4)

--- core/core-authentication/src/main/java/org/ghtk/todo_list/exception/OtpNotFoundException.java
+++ core/core-authentication/src/main/java/org/ghtk/todo_list/exception/OtpNotFoundException.java
@@ -2,10 +2,11 @@ package org.ghtk.todo_list.exception;
 
 import org.ghtk.todo_list.core_exception.exception.NotFoundException;
 
-public class OtpNotFoundException extends NotFoundException {
+public class OTPNotFoundException extends NotFoundException {
 
-  public OtpNotFoundException() {
+  public OTPNotFoundException(String email) {
     setStatus(404);
-    setCode("org.ghtk.todo_list.exception.OtpNotFoundException");
+    setCode("org.ghtk.todo_list.exception.OTPNotFoundException");
+    addParams("email", email);
   }
 }
