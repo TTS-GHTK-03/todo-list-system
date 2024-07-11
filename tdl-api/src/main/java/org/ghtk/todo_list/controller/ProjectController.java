23b090bc42e3b0d67ca42ab5cf675787b35890f8
PhanTrongKieu <PhanTrongKieu1401@users.noreply.github.com>
2024-07-11 16:19:03 +0700

    feat: fix comment api view project (#3)

--- tdl-api/src/main/java/org/ghtk/todo_list/controller/ProjectController.java
+++ tdl-api/src/main/java/org/ghtk/todo_list/controller/ProjectController.java
@@ -20,12 +20,14 @@ public class ProjectController {
     @GetMapping()
     public BaseResponse getAllProject(){
         log.info("(getAllProject)");
+        String userId = "5df2fe3e-c3d7-4236-8283-e62656a5fd24";
         return BaseResponse.of(HttpStatus.OK.value(), LocalDate.now().toString(), projectService.getAllProject(userId));
     }
 
     @GetMapping("/{project_id}")
     public BaseResponse getProject(@PathVariable(name = "project_id") String projectId){
         log.info("(getProject)projectId: {}", projectId);
+        String userId = "5df2fe3e-c3d7-4236-8283-e62656a5fd24";
         return BaseResponse.of(HttpStatus.OK.value(), LocalDate.now().toString(), projectService.getProject(userId, projectId));
     }
 }
