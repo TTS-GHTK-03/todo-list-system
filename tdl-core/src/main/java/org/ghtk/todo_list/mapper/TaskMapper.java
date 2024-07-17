545410cf1067f76b6bc8f1ea345a80b3d4755763
HanLuan09 <LuanHan1001@gmail.com>
2024-07-17 18:15:56 +0700

    feat: update logic api get status task (#3)

--- tdl-core/src/main/java/org/ghtk/todo_list/mapper/TaskMapper.java
+++ tdl-core/src/main/java/org/ghtk/todo_list/mapper/TaskMapper.java
@@ -3,10 +3,8 @@ package org.ghtk.todo_list.mapper;
 import java.util.List;
 import org.ghtk.todo_list.entity.Task;
 import org.ghtk.todo_list.model.response.TaskResponse;
-import org.ghtk.todo_list.model.response.UpdateDueDateTaskResponse;
 
 public interface TaskMapper {
-  UpdateDueDateTaskResponse toUpdateDueDateTaskResponse(Task task);
 
   List<TaskResponse> toTaskResponses(List<Task> tasks);
 
--- tdl-core/src/main/java/org/ghtk/todo_list/mapper/impl/TaskMapperImpl.java
+++ tdl-core/src/main/java/org/ghtk/todo_list/mapper/impl/TaskMapperImpl.java
@@ -21,4 +21,5 @@ public class TaskMapperImpl implements TaskMapper {
           .build();
     }).collect(Collectors.toList());
   }
+
 }
