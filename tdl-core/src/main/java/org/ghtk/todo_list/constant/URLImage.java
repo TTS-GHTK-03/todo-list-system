fde87311080301cae2928f86f32ce3e1459a2211
Linh Dev <nhoquenroi2002@gmail.com>
2024-07-19 13:51:26 +0700

    fix: update logic

---
+++ tdl-core/src/main/java/org/ghtk/todo_list/constant/URLImage.java
@@ -0,0 +1,8 @@
+package org.ghtk.todo_list.constant;
+
+public class URLImage {
+  public static final String URL_IMAGE_BUG = "URL_IMAGE_BUG";
+  public static final String URL_IMAGE_TASK = "URL_IMAGE_TASK";
+  public static final String URL_IMAGE_STORY = "URL_IMAGE_STORY";
+
+}
